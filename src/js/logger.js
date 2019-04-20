e17395e4af118e9b029a1293ed3ddec16736d090
ZengLiyuan <brainburster@outlook.com>
2019-04-20 23:35:27 +0800

    优化界面

--- src/js/logger.js
+++ src/js/logger.js
@@ -1,15 +1,13 @@
 /** Log */
 class Logger {
-  constructor (info) {
+  constructor(info) {
     this.info = info
   }
-  log (line1, line2, line3) {
+  log(line1, line2, line3) {
     if (line2 === undefined && line3 === undefined) {
-      // line2 = line1
-      // line1 = ' '
-      // line3 = ' '
-      this.info.innerText = `${line1}`
-      return
+      line2 = line1
+      line1 = ' '
+      line3 = ' '
     }
     this.info.innerText = `${line1}\n${line2}\n${line3}\n`
   }
